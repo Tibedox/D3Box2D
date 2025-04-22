e4c1da0d3331f6c1761e5c9e12cc4b12e61a0881
Samsung120 <Samsung@sch120.ru>
2025-04-22 21:33:42 +0600

    Box2D start

---
+++ core/src/main/java/ru/samsung/d3box2d/Main.java
@@ -0,0 +1,52 @@
+package ru.samsung.d3box2d;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    public static final float WORLD_WIDTH = 16, WORLD_HEIGHT = 9;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private World world;
+    private Box2DDebugRenderer debugRenderer;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, WORLD_WIDTH, WORLD_HEIGHT);
+        Box2D.init();
+        world = new World(new Vector2(0, -10), true);
+        debugRenderer = new Box2DDebugRenderer();
+        StaticBody floor = new StaticBody(world, 8, 0.5f, 15.5f, 0.4f);
+        StaticBody wall1 = new StaticBody(world, 1, 4.5f, 0.4f, 7);
+        StaticBody wall2 = new StaticBody(world, 15, 4.5f, 0.4f, 7);
+        DynamicBody[] ball = new DynamicBody[150];
+        for (int i = 0; i < ball.length; i++) {
+            ball[i] = new DynamicBody(world, 8+MathUtils.random(-0.1f, 0.1f), 5+i, 0.3f);
+        }
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        debugRenderer.render(world, camera.combined);
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.end();
+        world.step(1/60f, 6, 2);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
